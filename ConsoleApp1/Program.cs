422efeeac680b770c7a29777da3d0fd529a1bed4
artem <barishartem@gmail.com>
2022-11-02 23:48:17 +0200

    HM

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Runtime.Serialization;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using hometask;
+JSON json = new JSON();
+json.Serialization(new House(12, "Artem", new CPublishingHouse(5, "a", "a")));
+json.Deserialization();
